af22964761aa935e1e5ccebf3b9b5d76262fa934
SARAFAHMIDHA <109057805+SARAFAHMIDHA@users.noreply.github.com>
2022-12-21 20:50:07 +0530

    Add files via upload

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.frontend')
+
+@section('content')
+
+<h1>About</h1>
+<p>This is about page</p>
+@endsection
---
+++ resources/views/pages/optedcourse/index.blade.php
@@ -0,0 +1,104 @@
+@extends('layouts.frontend')
+
+@section('content')
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12 mt-4">
+        <div class="card">
+            <div class="cardheader">
+                <h4>tbl_student_opted_course</h4>
+            </div>
+            <div class="cardbody">
+
+            
+        <table class="table">
+         <thead>
+          <tr>
+      <th>id</th>
+      <th>Student_id</th>
+      <th>Course_id</th>
+      <th>Is_active</th>
+    
+    </tr>
+  </thead>
+  <tbody>
+  
+    <tr>
+      <td>1</td>
+      <th>1</th>
+      <th>1</th>
+      <th>0</th>
+</tr>
+<tr>
+      <td>2</td>
+      <th>1</th>
+      <th>4</th>
+      <th>0</th>
+</tr>
+<tr>
+      <td>3</td>
+      <th>2</th>
+      <th>1</th>
+      <th>0</th>
+</tr>
+<tr>
+      <td>4</td>
+      <th>3</th>
+      <th>2</th>
+      <th>0</th>
+</tr>
+<tr>
+      <td>5</td>
+      <th>3</th>
+      <th>4</th>
+      <th>0</th>
+</tr>
+<tr>
+      <td>6</td>
+      <th>4</th>
+      <th>2</th>
+      <th>0</th>
+</tr>
+<tr>
+      <td>7</td>
+      <th>4</th>
+      <th>1</th>
+      <th>0</th>
+</tr>
+<tr>
+      <td>8</td>
+      <th>4</th>
+      <th>3</th>
+      <th>0</th>
+</tr>
+<tr>
+      <td>9</td>
+      <th>5</th>
+      <th>3</th>
+      <th>0</th>
+</tr>
+<tr>
+      <td>10</td>
+      <th>6</th>
+      <th>2</th>
+      <th>0</th>
+</tr>
+<tr>
+      <td>11</td>
+      <th>7</th>
+      <th>1</th>
+      <th>0</th>
+</tr>
+
+  </tbody>
+</table>
+</div>
+        </div>
+        </div>
+
+    </div>
+
+</div>
+
+@endsection
