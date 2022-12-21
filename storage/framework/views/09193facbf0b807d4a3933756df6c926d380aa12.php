2ef6d620b5fc7c14a2fa53635ede95f579fa7fa8
SARAFAHMIDHA <109057805+SARAFAHMIDHA@users.noreply.github.com>
2022-12-21 20:51:20 +0530

    Add files via upload

---
+++ storage/framework/views/09193facbf0b807d4a3933756df6c926d380aa12.php
@@ -0,0 +1,60 @@
+
+
+<?php $__env->startSection('content'); ?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12 mt-4">
+        <div class="card">
+            <div class="cardheader">
+                <h4>tbl_course</h4>
+            </div>
+            <div class="cardbody">
+
+            
+        <table class="table">
+         <thead>
+          <tr>
+      <th>id</th>
+      <th>Course name</th>
+      <th>dept</th>
+    
+    </tr>
+  </thead>
+  <tbody>
+  
+    <tr>
+      <td>1</td>
+      <th>English</th>
+      <th>English</th>
+      </tr>
+      <tr>
+      <td>2</td>
+      <th>Mathematics</th>
+      <th>Maths</th>
+      </tr>
+      <tr>
+      <td>3</td>
+      <th>Science</th>
+      <th>Science</th>
+      </tr>
+      <tr>
+      <td>4</td>
+      <th>Economics</th>
+      <th>Science</th>
+      </tr>
+    
+  </tbody>
+</table>
+</div>
+        </div>
+        </div>
+
+    </div>
+
+</div>
+
+<?php $__env->stopSection(); ?>
+
+
+<?php echo $__env->make('layouts.frontend', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\fahmi\OneDrive\Desktop\laravelapp\app\resources\views/pages/course/index.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/d9a2744c3f784f3623d6148a934fc3ec22ab2aa0.php
@@ -0,0 +1,1 @@
+<h1><?php echo e($name); ?></h1><?php /**PATH C:\Users\fahmi\OneDrive\Desktop\laravelapp\app\resources\views/pages/users.blade.php ENDPATH**/ ?>
